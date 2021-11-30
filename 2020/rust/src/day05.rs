3066bb9d31cef4d286de8fbd75d2ed1fbf75ffcb
Erik Byström <erik.bystrom@gmail.com>
2021-11-30 14:45:17 +0100

    2020.05

---
+++ 2020/rust/src/day05.rs
@@ -0,0 +1,27 @@
+pub fn run() {
+    println!("=== Day 05 ===");
+
+    let mut seat_ids = include_str!("../../input/input5.txt")
+        .lines()
+        .map(|l| l.chars()
+            .map(|x| x as i32)
+            .fold(0, |a, x| (a<<1)+((x%7)%2))
+        )
+        .collect::<Vec<i32>>();
+
+    seat_ids.sort();
+
+    println!("A: {}", seat_ids.get(seat_ids.len()-1).unwrap());
+
+    let mut p2: i32 = 0;
+    let mut p1: i32 = 0;
+
+    for seat_id in seat_ids {
+        if seat_id - p2 == 3 {
+            println!("B: {}", p1+1);
+            break;
+        }
+        p2 = p1;
+        p1 = seat_id;
+    }
+}
