115959a1f7ecd92d9a73d601e675599a1c0529ef
Erik Byström <erik.bystron@noidgames.com>
2022-12-06 08:37:41 +0100

    2022.06

--- 2022/python/day06.py
+++ 2022/python/day06.py
@@ -6,31 +6,35 @@ import itertools
 
 DAY = "06"
 INPUT = "../input/input{}.txt".format(DAY)
-INPUT = "../input/test.txt"
+#INPUT = "../input/test.txt"
 
 with open(INPUT) as fd:
     data = fd.read()
 
 
+def has_dup(buffer):
+    dup = [0] * 32
+    for ch in buffer:
+        p = ord(ch) - ord('a')
+        if dup[p] != 0:
+            return True
+        dup[p] += 1
+    return False
+
+
 def find(data, size):
     buf = [0] * size
-    ind = [0] * size
-    dup = [0] * size
     j = 0
 
     for i in range(len(data)):
         ch = data[i]
-        buf.index(ch)
-        dup[j] = 1 if ch in buf else 0
         buf[j] = ch
         j = (j + 1) % size
-        if i < size or 1 in dup:
+        if i < size or has_dup(buf):
             continue
-        return i
-
-    if 1 not in dup:
-        return len(data)
+        return i + 1
     return -1
 
+
 print("A:",  find(data, 4))
-#print("B:", find(data, 14))
+print("B:", find(data, 14))
