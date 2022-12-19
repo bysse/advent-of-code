4fe005880afcc8bc195d11cb2ca0b0418875ee8b
Erik Byström <erik.bystrom@gmail.com>
2022-12-19 11:57:38 +0100

    2022.19 start

--- 2022/python/day19.py
+++ 2022/python/day19.py
@@ -6,14 +6,33 @@ import itertools
 
 DAY = "19"
 INPUT = f"../input/input{DAY}.txt"
-#INPUT = "../input/test.txt"
+INPUT = "../input/test.txt"
 
-data = {}
+blueprints = {}
 for line in lines(INPUT):
     id, ore, clay, oo, oc, go, gc = ints(line)
-    data[id] = ((ore, 0), (clay, 0), (oo, oc), (go, gc))
+    blueprints[id] = ((ore, 0), (clay, 0), (oo, oc), (go, gc))
 
-print(data)
+
+def dump(state):
+    s, r = state
+    print(f"{s[0]} {s[1]} {s[2]} {s[3]} | {r[0]} {r[1]} {r[2]} {r[3]}")
+
+
+def produce(stuff, robots):
+    for i in range(4):
+        stuff[i] += robots[i]
+    return stuff
+
+
+for id, bp in blueprints.items():
+    print(id, bp)
+    # do DFS
+
+
+
+
 
 print("A:")
 print("B:")
+
