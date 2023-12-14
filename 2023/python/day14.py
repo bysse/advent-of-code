f5d45570b9678044239829851b8931e8238bd3f6
Erik Byström <erik.bystrom@gmail.com>
2023-12-14 13:18:18 +0100

    2023-14b

--- 2023/python/day14.py
+++ 2023/python/day14.py
@@ -6,7 +6,7 @@ import itertools
 
 DAY = extract(os.path.basename(__file__), r"(\d+)")[0]
 INPUT = f"../input/input{DAY}.txt"
-INPUT = f"../input/test{DAY}.txt"
+#INPUT = f"../input/test{DAY}.txt"
 
 A = 0
 B = 0
@@ -16,7 +16,30 @@ for line in lines(INPUT):
     data.append(list(line))
 
 
-def iterate_north(data):
+def part_a(data):
+    while tilt_north(data) > 0:
+        pass
+    return weight(data)
+
+
+def part_b(data):
+    for i in range(0, 100):
+        cycle(data)
+        w = weight(data)
+    return w
+
+def cycle(data):
+    while tilt_north(data):
+        pass
+    while tilt_west(data):
+        pass
+    while tilt_south(data):
+        pass
+    while tilt_east(data):
+        pass
+
+
+def tilt_north(data):
     changed = 0
     for y in range(1, len(data)):
         target = data[y-1]
@@ -31,7 +54,7 @@ def iterate_north(data):
     return changed
 
 
-def iterate_south(data):
+def tilt_south(data):
     changed = 0
     for y in range(len(data) - 2, -1, -1):
         target = data[y+1]
@@ -46,7 +69,7 @@ def iterate_south(data):
     return changed
 
 
-def iterate_east(data):
+def tilt_east(data):
     changed = 0
     for line in data:
         for x in range(len(data[0]) - 2, -1, -1):
@@ -59,7 +82,7 @@ def iterate_east(data):
     return changed
 
 
-def iterate_west(data):
+def tilt_west(data):
     changed = 0
     for line in data:
         for x in range(1, len(data[0])):
@@ -72,24 +95,15 @@ def iterate_west(data):
     return changed
 
 
-def cycle(data):
-    changed = 0
-    changed += iterate_north(data)
-    changed += iterate_west(data)
-    changed += iterate_south(data)
-    changed += iterate_east(data)
-    return changed
+def weight(data):
+    w = 0
+    height = len(data)
+    for y, row in enumerate(data):
+        for x, ch in enumerate(row):
+            if ch == 'O':
+                w += height - y
+    return w
 
 
-cycle(data)
-dump_2d(data)
-
-height = len(data)
-for y, row in enumerate(data):
-    for x, ch in enumerate(row):
-        if ch == 'O':
-            A += height - y
-
-
-print("A:", A)
-print("B:", B)
+print("A:", part_a(copy.deepcopy(data)))
+print("B:", part_b(copy.deepcopy(data)))
