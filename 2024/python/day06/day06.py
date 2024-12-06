4f043030adf92b85aaf000cbec639a80e9038a0d
Erik Byström <erik.bystrom@gmail.com>
2024-12-06 08:36:59 +0100

    2024.06 [start]

---
+++ 2024/python/day06/day06.py
@@ -0,0 +1,89 @@
+from collections import defaultdict
+
+from std import *
+import copy
+import re
+import functools
+import itertools
+
+
+def main(input_file):
+    pos = (0, 0)
+    start = (0,0)
+    field = []
+    for (y, line) in enumerate(lines(input_file)):
+        field.append(line)
+        if "^" in line:
+            start = (line.index("^"), y)
+
+    pos = start
+    delta = (0, -1)
+
+    width = len(field[0])
+    height = len(field)
+
+    visited = set()
+    intersections = defaultdict(lambda: set())
+
+    max_length = 0
+    while True:
+        visited.add(pos)
+        np = (pos[0] + delta[0], pos[1] + delta[1])
+        if not (0 <= np[0] < width and 0 <= np[1] < height):
+            break
+
+        if field[np[1]][np[0]] == "#":
+            delta = rotate_right(delta)
+            continue
+        else:
+            pos = np
+            max_length += 1
+
+        if pos in visited:
+            intersections[pos].add(delta)
+
+
+    print("A:", len(visited))
+
+    for pos, dirs in intersections.items():
+        for delta in dirs:
+            blocker = (pos[0] + delta[0], pos[1] + delta[1])
+            print(blocker)
+            if detect_loop(field, start, (0, -1), blocker, 2*max_length):
+                print("loop:", pos)
+        pass
+
+
+def detect_loop(field, pos, delta, extra, max_length):
+    width = len(field[0])
+    height = len(field)
+
+    steps = 0
+    while True:
+        if steps >= max_length:
+            return True
+
+        np = (pos[0] + delta[0], pos[1] + delta[1])
+        if not (0 <= np[0] < width and 0 <= np[1] < height):
+            break
+
+        if field[np[1]][np[0]] == "#" or np == extra:
+            delta = rotate_right(delta)
+        else:
+            pos = np
+        steps += 1
+    return False
+
+def rotate_right(delta):
+    if delta == (0, -1):
+        return 1, 0
+    if delta == (1, 0):
+        return 0, 1
+    if delta == (0, 1):
+        return -1, 0
+    if delta == (-1, 0):
+        return 0, -1
+
+if __name__ == "__main__":
+    #main("input.txt")
+    main("test.txt")
