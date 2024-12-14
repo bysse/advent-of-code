80b9659a53ccedc3d0bfb35b0adfc5dea190abcd
Erik Byström <erik.bystrom@gmail.com>
2024-12-14 20:36:53 +0100

    2024.14a

---
+++ 2024/python/day14/day14.py
@@ -0,0 +1,77 @@
+import pygame
+
+from std import *
+
+
+def evaluate(robots, turns, width, height):
+    pos = set()
+    for robot in robots:
+        x = (robot[0] + turns * robot[2]) % width
+        y = (robot[1] + turns * robot[3]) % height
+        pos.add((x, y))
+    return pos
+
+
+def is_tree(pos, width, height):
+    margin = 5
+    for x, y in pos:
+        if margin < x < width-margin:
+            continue
+        return False
+    return True
+
+
+def main(input_file):
+    robots = []
+    for line in lines(input_file):
+        robots.append(ints(line))
+
+    width = 101
+    height = 103
+    turns = 100
+    cx = width // 2
+    cy = height // 2
+
+    quadrant = [0, 0, 0, 0]
+
+    for robot in robots:
+        x = (robot[0] + turns * robot[2]) % width
+        y = (robot[1] + turns * robot[3]) % height
+        if x == cx or y == cy:
+            continue
+        index = (0 if x < cx else 1) + (0 if y < cy else 2)
+        quadrant[index] += 1
+
+    A = quadrant[0] * quadrant[1] * quadrant[2] * quadrant[3]
+    print("A:", A)
+
+    pygame.init()
+    window = pygame.display.set_mode((2 * width, 2 * height))
+
+    turns = 0
+    run = True
+    while run:
+        turns += 2
+        print(turns)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pos = evaluate(robots, turns, width, height)
+        if not is_tree(pos, width, height):
+            continue
+
+        window.fill(0)
+        rect = pygame.Rect(window.get_rect().center, (0, 0)).inflate(*([min(window.get_size())//2]*2))
+        for x, y in pos:
+            window.set_at((rect.left + x, rect.top + y), 0xff)
+
+        pygame.image.save(window, f"frame_{turns}.png")
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main("input.txt")
+    # main("test.txt")
